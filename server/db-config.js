0f0eb4ca46b8bf65aaf57955d6ab61ba0a6c33d7
jariwbh <jariwbh@gmail.com>
2017-07-13 13:05:39 +0530

    Update db-config.js

--- server/db-config.js
+++ server/db-config.js
@@ -1,3 +1,3 @@
 var mongoose   = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/datacenter'); // connect to our database
+mongoose.connect('mongodb://dbuser:pass#123@ds157702.mlab.com:57702/heroku_nx3z5524'); // connect to our database
